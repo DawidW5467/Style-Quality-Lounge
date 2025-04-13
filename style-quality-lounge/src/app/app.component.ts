5245782770bfe84566b0f9dc1e4be35793f3c6ae
DawidW5467 <dawidwoj08@onet.eu>
2025-04-13 21:28:25 +0200

    CHOPPED LOGIN REGISTER

--- style-quality-lounge/src/app/app.component.ts
+++ style-quality-lounge/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import {LoginComponent} from './login/login.component';
-
+import {LoginComponent} from './login/login.component';;
+import {FooterComponent} from './footer/footer.component';
+import {RegisterComponent} from './register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, FooterComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
